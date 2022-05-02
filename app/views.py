b525127aebb0c58fe54a6bec205974e628c96b86
ABBY <abbyW@moringa.com>
2022-05-02 22:04:22 +0300

    Created a route and instance of all articles in the verge news source.

--- app/views.py
+++ app/views.py
@@ -161,6 +161,35 @@ def espn():
 
     return render_template('espn.html',contents=contents)
 
+@app.route('/verge')
+def verge():
+
+    newsapi = NewsApiClient(api_key='d3afaf05f0a744be8752d41b2ad1d279')
+    all_articles = newsapi.get_everything(sources = 'the-verge')
+
+    a_articles = all_articles['articles']
+
+    news_all = []
+    desc_all = []
+    img_all = []
+    p_date_all = []
+    url_all = []
+
+    for i in range(len(a_articles)):
+        all_article = a_articles[i]
+
+        news_all.append (all_article['title'])
+        desc_all.append(all_article['description'])
+        img_all.append(all_article['urlToImage'])
+        p_date_all.append(all_article['publishedAt'])
+        url_all.append(all_article['url'])
+
+        contents = zip(news_all,desc_all,img_all,p_date_all,url_all)
+
+
+    return render_template('verge.html',contents=contents)
+
+
 
 
 if __name__ == '__main__':
