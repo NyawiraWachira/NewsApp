194a16fcfb67d0ceb02c4cf994ccaa2d8188d34d
ABBY <abbyW@moringa.com>
2022-05-02 22:25:25 +0300

    Added bootstrap cdn link to project.

--- app/views.py
+++ app/views.py
@@ -1,15 +1,5 @@
-# api_key = 'd3afaf05f0a744be8752d41b2ad1d279'
-# d3afaf05f0a744be8752d41b2ad1d279
-# https://newsapi.org/v2/everything?q=Apple&from=2022-05-02&sortBy=popularity&apiKey=API_KEY
-#url = ('https://newsapi.org/v2/everything?'
-    #    'q=Apple&'
-    #    'from=2022-05-02&'
-    #    'sortBy=popularity&'
-    #    'apiKey=d3afaf05f0a744be8752d41b2ad1d279')
-# https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY
-
 from flask import Flask, render_template
-# from app import app
+
 
 from newsapi import NewsApiClient
 
