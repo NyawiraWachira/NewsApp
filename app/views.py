ad60b7ce81ffaecd08c8f080156cc34b5a98067d
ABBY <abbyW@moringa.com>
2022-05-02 21:14:32 +0300

    Created news source model and route to home page.

--- app/views.py
+++ app/views.py
@@ -19,6 +19,36 @@ app = Flask(__name__)
 
 
 
-# 
+@app.route('/')
+def index():
+
+    newsapi = NewsApiClient(api_key='d3afaf05f0a744be8752d41b2ad1d279')
+    top_headlines = newsapi.get_top_headlines(sources = 'bbc-news, buzzfeed, al-jazeera-english, bbc-sport, the-verge')
+    
+
+    t_articles = top_headlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(t_articles)):
+        main_article = t_articles[i]
+
+        news.append (main_article['title'])
+        desc.append(main_article['description'])
+        img.append(main_article['urlToImage'])
+        p_date.append(main_article['publishedAt'])
+        url.append(main_article['url'])
+
+        contents = zip(news,desc,img,p_date,url)
+
+    return render_template('index.html',contents=contents)
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
