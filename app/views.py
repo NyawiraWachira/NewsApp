bc5afe9fd97076b3b94f3d1c4f7dcfa730efda35
ABBY <abbyW@moringa.com>
2022-05-02 21:49:44 +0300

    Created a route and  instance of all articles in the aljazeera news source.

--- app/views.py
+++ app/views.py
@@ -76,6 +76,35 @@ def bbc():
     return render_template('bbc.html',contents=contents)
 
 
+@app.route('/aljazeera')
+def aljazeera():
+
+    newsapi = NewsApiClient(api_key='d3afaf05f0a744be8752d41b2ad1d279')
+    all_articles = newsapi.get_everything(sources = 'al-jazeera-english')
+
+    a_articles = all_articles['articles']
+
+    news_all = []
+    desc_all = []
+    img_all = []
+    p_date_all = []
+    url_all = []
+
+    for i in range(len(a_articles)):
+        all_article = a_articles[i]
+
+        news_all.append (all_article['title'])
+        desc_all.append(all_article['description'])
+        img_all.append(all_article['urlToImage'])
+        p_date_all.append(all_article['publishedAt'])
+        url_all.append(all_article['url'])
+
+        contents = zip(news_all,desc_all,img_all,p_date_all,url_all)
+
+
+    return render_template('aljazeera.html',contents=contents)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
