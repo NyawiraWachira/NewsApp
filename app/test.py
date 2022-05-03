6a4515755286f32ba7c6426f1e9f0dcb42fb18df
ABBY <abbyW@moringa.com>
2022-05-03 15:44:03 +0300

    Confirmed test for news articles class.

--- app/test.py
+++ app/test.py
@@ -1,11 +1,35 @@
 import unittest
-
+from models import article
 from models import source
 
-
+Article = article.Article
 Source = source.Source
 
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
 
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Article('Roe v Wade: US Supreme Court may overturn abortion rights, leak suggests - BBC','Millions could lose legal rights to abortion this summer, a leaked Supreme Court document shows.',
+        'https://ichef.bbci.co.uk/news/1024/branded_news/A0E5/production/_124398114_gettyimages-1395079914.jpg','2022-05-03T10:08:35Z','https://www.bbc.com/news/world-us-canada-61302740')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_article.title,"Roe v Wade: US Supreme Court may overturn abortion rights, leak suggests - BBC")
+        self.assertEqual(self.new_article.description,"Millions could lose legal rights to abortion this summer, a leaked Supreme Court document shows.")
+        self.assertEqual(self.new_article.urlToImage,"https://ichef.bbci.co.uk/news/1024/branded_news/A0E5/production/_124398114_gettyimages-1395079914.jpg")
+        self.assertEqual(self.new_article.publishedAt,"2022-05-03T10:08:35Z")
+        self.assertEqual(self.new_article.url,"https://www.bbc.com/news/world-us-canada-61302740")
 
 class SourceTest(unittest.TestCase):
     '''
